d5caea89fa10ef197f250ac3eed0557f900c04c2
elmon877 <waliyulmukarramah877@gmail.com>
2026-07-13 18:50:38 +0700

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import { AuthProvider } from './context/AuthContext'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
